549ff22c86239fefde10b1d6797200bffcb049ca
Leonardo Paini <leonardopaini@gmail.com>
2019-09-10 16:55:40 -0300

    Creating our first components

---
+++ src/components/Inventory.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Inventory extends React.Component {
+  render() {
+    return <div className="inventory">I'm the inventory!</div>;
+  }
+}
+
+export default Inventory;
---
+++ src/components/Order.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Order extends React.Component {
+  render() {
+    return <div className="order">This is the order!</div>;
+  }
+}
+
+export default Order;
