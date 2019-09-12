123c0188f0acae876b2beebf6906c7307efd2438
Leonardo Paini <leonardopaini@gmail.com>
2019-09-12 10:19:59 -0300

    update references

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,11 +3,11 @@ import React from "react";
 class StorePicker extends React.Component {
   render() {
     return (
-      <form className="store-picker">
+      <form className="store-selector">
         <h2>Please enter a Store</h2>
         <input type="text" required placeholder="Store Name" />
 
-        <button type="submit">Visit Store</button>
+        <button type="submit">Visit Store →</button>
       </form>
     );
   }
